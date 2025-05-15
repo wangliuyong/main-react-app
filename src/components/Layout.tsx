f82a7141197d54d7e32f7b76e318da785c39a124
liuyong.wang.o <liuyonng.wang.o@nio.com>
2025-05-15 16:33:56 +0800

    feat: 重构布局，整合菜单组件，删除冗余代码，优化样式

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,68 @@
-import Menu from "./Menu";
 import "./layout.scss";
+import {
+  ContainerOutlined,
+  DesktopOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
 
-function MenuLayout() {
+import React, { useState } from "react";
+
+import { Button, Layout, Menu, theme } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
-    <>
-      <div className="container">
-        <div className="slider-wrap">
-          <Menu />
-        </div>
-        <div className="app-container">
-          <div className="header">header</div>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            { key: "1", icon: <PieChartOutlined />, label: "首页" },
+            { key: "2", icon: <DesktopOutlined />, label: "登录页" },
+            { key: "3", icon: <ContainerOutlined />, label: "微应用VUE3" },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
           <div className="sub-wrap" id="sub-vue-app">
             loading...
           </div>
-        </div>
-      </div>
-    </>
+        </Content>
+      </Layout>
+    </Layout>
   );
-}
+};
 
-export default function Layout() {
-  return <MenuLayout></MenuLayout>;
-}
+export default App;
