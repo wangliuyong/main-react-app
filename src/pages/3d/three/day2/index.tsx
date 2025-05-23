d9040b28f513a230fb40b19c30686baf4a28badb
liuyong.wang.o <liuyonng.wang.o@nio.com>
2025-05-23 16:12:42 +0800

    fix: 优化three.js场景初始化，调整光源和相机设置，添加旋转动画

--- src/pages/3d/three/day2/index.tsx
+++ src/pages/3d/three/day2/index.tsx
@@ -5,6 +5,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default function App() {
   const renderer = new THREE.WebGLRenderer();
+
   const initThree = () => {
     const domContainer = document.getElementById("three-container");
     if (domContainer) {
@@ -19,37 +20,55 @@ export default function App() {
         3000
       );
 
+      // 生成网格并添加到场景中
+      // 立方体几何体
+      const geometry = new THREE.BoxGeometry(16, 16, 16);
+      // 标准材质
+      const material = new THREE.MeshLambertMaterial({
+        color: 0xff0000,
+      });
+      // 网格
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.set(10, 0, 0);
+      scene.add(mesh);
+
+      const render = () => {
+        renderer.render(scene, camera);
+      };
+
       const initCamera = () => {
         // 设置摄像机位置
-        camera.position.set(32, 32, 32);
+        camera.position.set(-32, 32, 32);
         camera.lookAt(0, 0, 0);
       };
 
-      // 光源
+      // 光源 (点光源，平行光，环境光，聚光灯光源),默认都指向坐标原点
       function initLight() {
         // 点光源
-        const point = new THREE.DirectionalLight(0xffffff);
-        point.position.set(80, 100, 80);
+        const point = new THREE.PointLight(0xffffff);
+        point.position.set(20, 20, 20);
+        point.lookAt(-32, 32, 32);
         scene.add(point);
 
-        // 环境光
-        const light = new THREE.AmbientLight(0xffffff, 0.1);
-        light.position.set(0, 0, 0);
-        scene.add(light);
-      }
+        const pointHelper = new THREE.PointLightHelper(point, 1);
+        scene.add(pointHelper);
 
-      // 生成网格并添加到场景中
-      function initMesh() {
-        // 立方体几何体
-        const geometry = new THREE.BoxGeometry(16, 16, 16);
-        // 标准材质
-        const material = new THREE.MeshStandardMaterial({
-          color: 0xff0000,
-        });
-        // 网格
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(0, 0, 0);
-        scene.add(mesh);
+        // 平行光;
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(30, 30, 30);
+        scene.add(directionalLight);
+
+        // 平行光辅助对象
+        const directionalLightHelper = new THREE.DirectionalLightHelper(
+          directionalLight,
+          2
+        );
+        scene.add(directionalLightHelper);
+
+        // 环境光
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+        ambientLight.position.set(0, 0, 0);
+        scene.add(ambientLight);
       }
 
       // 坐标系
@@ -59,17 +78,28 @@ export default function App() {
         scene.add(axes);
       };
 
-      initMesh();
+      // 相机轨道控制
+      const initCameraControl = () => {
+        const controls = new OrbitControls(camera, renderer.domElement); //创建控件对象
+        controls.addEventListener("change", render);
+      };
+
+      // 旋转动画
+      const initRoate = () => {
+        // 旋转
+        const animate = () => {
+          mesh.rotateY(0.1);
+          render();
+          requestAnimationFrame(animate);
+        };
+        animate();
+      };
+
       initLight();
       initCamera();
       initAxios();
-
-      const render = () => {
-        renderer.render(scene, camera);
-      };
-
-      const controls = new OrbitControls(camera, renderer.domElement); //创建控件对象
-      controls.addEventListener("change", render);
+      initCameraControl();
+      initRoate();
 
       renderer.setSize(domContainer.clientWidth, domContainer.clientHeight);
       renderer.render(scene, camera);
@@ -77,16 +107,19 @@ export default function App() {
       domContainer.appendChild(renderer.domElement);
     }
   };
+
   useEffect(() => {
-    const domContainer = document.getElementById("three-container");
     initThree();
     return () => {
+      const domContainer = document.getElementById("three-container");
       // 组件卸载时清理
       domContainer?.removeChild(renderer.domElement);
     };
   }, []);
 
   window.addEventListener("resize", () => {
+    const domContainer = document.getElementById("three-container");
+    domContainer?.removeChild(renderer.domElement);
     initThree();
   });
 
